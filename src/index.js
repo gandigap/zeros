0be5dc5ecd93429203c6c03b8f7e6935b7026abe
gandigap <igor.simatic@gmail.com>
2018-09-30 09:45:45 +0300

    zeros commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let zeros = 0;
+  for(let i = 5; number / i > 1; i = i * 5) {
+    zeros += Math.floor(number / i);
+  }
+  return zeros;
 }
